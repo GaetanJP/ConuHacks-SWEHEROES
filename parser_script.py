d986ac6ca2aef06106568cbea79c8588da0d82ce
GreenPep <daniel.zhang24@hotmail.com>
2024-01-20 12:46:02 -0500

    hardcoded the files

--- parser_script.py
+++ parser_script.py
@@ -2,13 +2,30 @@ import sys
 import json
 
 
-def parse(json_file):
+def parse(json_file1, json_file2, json_file3):
     parsed_information = {}
-    with open(json_file, "r") as file:
-        data = json.load(file)
-        return data
+    data1 = None
+    data2 = None
+    data3 = None
+
+    with open(json_file1, "r") as file:
+        data1 = json.load(file)
+        file.close()
+
+    with open(json_file2, "r") as file:
+        data2 = json.load(file)
+        file.close()
+
+    with open(json_file3, "r") as file:
+        data3 = json.load(file)
+        file.close()
+
+    return [data1, data2, data3]
 
 
 if __name__ == "__main__":
-    exchange_json = sys.argv[1]
-    parse(exchange_json)
+    parse(
+        "./dataset/Exchange_1.json",
+        "./dataset/Exchange_2.json",
+        "./dataset/Exchange_3.json",
+    )
